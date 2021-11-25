cddf627e7431dd136128aa386ae800bcabf4662b
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-11-26 00:59:46 +0100

    Ejercicios 6-7

--- Sesion 1 Scripts/GameManager.cs
+++ Sesion 1 Scripts/GameManager.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    GameObject e;
-    
+    GameObject[] e;
+    GameObject[] e2;
+
     // Start is called before the first frame update
     void Start()
     {
-        e = GameObject.FindGameObjectWithTag("fighter");
-        Debug.Log(e);
-        Fighter fighter = e.GetComponent<Fighter>();
-        Debug.Log("El nombre: " + fighter.Name);
-        Debug.Log("La categoria: " + fighter.Category);
-        Debug.Log("La vida: " + fighter.Health);
+        e = GameObject.FindGameObjectsWithTag("fighter");
+
+        List<string> listName = new List<string> { };
+        listName.Add("Scorpion");
+        listName.Add("Kano");
+        listName.Add("Sonya");
+        listName.Add("Johnny Cage");
+        listName.Add("Sub-Zero");
+
+        List<string> listCategory = new List<string> { };
+        listCategory.Add("Ninja");
+        listCategory.Add("Mercenario");
+        listCategory.Add("Teniente");
+        listCategory.Add("Actor");
+        listCategory.Add("Ninja");
+
+        e2 = InitFighters(e, listName, listCategory);
+        e = e2;
+
+        foreach (GameObject fighter in e)
+        {
+            Debug.Log("["+fighter.name+"]"+"El nombre: " + fighter.GetComponent<Fighter>().Name+"; la categoria: " +
+                fighter.GetComponent<Fighter>().Category+"; y la vida: " + fighter.GetComponent<Fighter>().Health);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    GameObject[] InitFighters(GameObject[] e, List<string> listName, List<string> listCategory)
+    {
+        for(int i=0; i < 5; i++)
+        {
+            e[i].GetComponent<Fighter>().Name = listName[i];
+            e[i].GetComponent<Fighter>().Category = listCategory[i];
+            e[i].GetComponent<Fighter>().Health = 100;
+        }
+
+        return e;
     }
 }
