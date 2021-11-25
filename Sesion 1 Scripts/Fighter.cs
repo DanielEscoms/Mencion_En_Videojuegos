d1575c4e9f60ecadd9d6beec9f442421fd1dfe55
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-11-26 00:59:14 +0100

    Ejercicios 4-5

--- Sesion 1 Scripts/Fighter.cs
+++ Sesion 1 Scripts/Fighter.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Fighter : MonoBehaviour
 {
-    public string Name = "Scorpion";
-    public string Category = "Ninja";
-    public int Health = 95;
+    public string Name;
+    public string Category;
+    public int Health;
 
     // Start is called before the first frame update
     void Start()
