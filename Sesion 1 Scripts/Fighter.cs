79f54b7b12a34a18ad99d5b3c49dd8133c46616e
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-11-25 20:51:39 +0100

    Ejercicios 1-3

---
+++ Sesion 1 Scripts/Fighter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public string Name = "Scorpion";
+    public string Category = "Ninja";
+    public int Health = 95;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Sesion 1 Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject e;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        e = GameObject.FindGameObjectWithTag("fighter");
+        Debug.Log(e);
+        Fighter fighter = e.GetComponent<Fighter>();
+        Debug.Log("El nombre: " + fighter.Name);
+        Debug.Log("La categoria: " + fighter.Category);
+        Debug.Log("La vida: " + fighter.Health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
