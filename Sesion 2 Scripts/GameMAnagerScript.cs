9fd14fd561e9d67c650ef2c17a85b95154ce52d9
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-12-02 18:57:30 +0100

    ejercicio 2 actividades 1 y 2

---
+++ Sesion 2 Scripts/GameMAnagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMAnagerScript : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject nuevaCarta = Instantiate(myPrefab, new Vector3(-8, 3, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
