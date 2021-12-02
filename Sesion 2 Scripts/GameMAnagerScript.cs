1f338e5ee2b5ed93fdc742bf980b0e21b72a5710
Daniel Escoms <daesdo@floridauniversitaria.es>
2021-12-02 19:32:09 +0100

    ejercicio 2 actividades 3 y 4

--- Sesion 2 Scripts/GameMAnagerScript.cs
+++ Sesion 2 Scripts/GameMAnagerScript.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class GameMAnagerScript : MonoBehaviour
 {
     public GameObject myPrefab;
+    List<GameObject> cardList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject nuevaCarta = Instantiate(myPrefab, new Vector3(-8, 3, 0), Quaternion.identity);
+        float posX = -8;
+        float posY = 3;
+
+        for (int i=1; i < 11; i++)
+        {
+            GameObject nueva_carta = Instantiate(myPrefab, new Vector3(posX, posY, 0), Quaternion.identity);
+            nueva_carta.name = "Carta" + i;
+            cardList.Add(nueva_carta);
+
+            posX += 3;
+            if (i == 5) {
+                posX = -8;
+                posY = -3;
+            }
+            
+            
+        }
+        
     }
 
     // Update is called once per frame
