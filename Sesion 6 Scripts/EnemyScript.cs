ba275c309d2dd2210035b0ccab23e24060f6af08
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-01-25 13:36:26 +0100

    Ejercicio 3

---
+++ Sesion 6 Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    bool directionRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("MoveEnemy", 0.5f, 0.5f); // Llama a la función MoveEnemy, a los 0,5sec y vuelve a llarmarla a los 0.5sec
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.8f);
+        MoveEnemy();
+        StartCoroutine(Wait());
+    }
+
+    void MoveEnemy()
+    {
+        if (directionRight == true) {
+            transform.Translate(Vector3.right);
+            if (transform.position.x > 6.5)
+            {
+                directionRight = false;
+            }
+        }
+        else {
+            transform.Translate(Vector3.left);
+            if (transform.position.x < -6.5)
+            {
+                directionRight = true;
+            }
+        }
+    }
+}
