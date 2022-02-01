ff22b62db33f86bc7db090df5ffc58a9a35b97af
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-01 22:36:28 +0100

    Ejercicio 5

--- Sesion 7 Scripts/EnemyScript.cs
+++ Sesion 7 Scripts/EnemyScript.cs
@@ -56,4 +56,14 @@ public class EnemyScript : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Torpedo")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
 }
