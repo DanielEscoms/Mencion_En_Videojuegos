25b631e3fa0a21a7a47601ca11a9310ce9ad7a93
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-01 22:17:16 +0100

    Ejercicio 4

---
+++ Sesion 7 Scripts/EnemyScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    bool directionRight = true;
+    float sleep = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("MoveEnemy", 0.5f, 0.5f); // Llama a la función MoveEnemy, a los 0,5sec y vuelve a llarmarla a los 0.5sec
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(sleep);
+        MoveEnemy();
+        StartCoroutine(Wait());
+    }
+
+    void MoveEnemy()
+    {
+        if (directionRight) {
+            
+            if (transform.position.x > 5.5)
+            {
+                sleep -= 0.05f;
+                directionRight = false;
+                transform.Translate(Vector3.down);
+            }
+            else
+            {
+                transform.Translate(Vector3.right);
+            }
+        }
+        else {
+            
+            if (transform.position.x < -5.5)
+            {
+                sleep -= 0.05f; 
+                directionRight = true;
+                transform.Translate(Vector3.down);
+            }
+            else
+            {
+                transform.Translate(Vector3.left);
+            }
+        }
+    }
+}
---
+++ Sesion 7 Scripts/ShotScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    GameObject spaceShip;
+    SpaceShipScript scriptNave;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceShip = GameObject.FindGameObjectWithTag("Player");
+        scriptNave = spaceShip.GetComponent<SpaceShipScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // aquí está el objeto contra el que choca, en este caso al poner el script en el torpedo el objeto será el TriggerTop (el gatillo superior)
+    {
+        Debug.Log("SHOT: He chocado con " + collision.gameObject.name);
+        scriptNave.SetCanShot();
+        Destroy(gameObject);
+    }
+}
