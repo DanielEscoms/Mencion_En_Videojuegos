ec6c48add678278ffee73b654da5c8c29a58fe8e
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-09 23:38:18 +0100

    Ejercicio 5

--- Sesion 8/Scripts/PlayerController.cs
+++ Sesion 8/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D myRB;
     public float speed;
+    GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -18,7 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         float move = Input.GetAxis("Horizontal");
         myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
-
+        if(move < 0)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (move > 0)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = false;
+        }
 
     }
 }
