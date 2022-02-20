719f26b9022d98de094a4474769cd2fbefa87449
Daniel Escoms <daesdo@floridauniversitaria.es>
2022-02-20 16:24:13 +0100

    Ejercicio 7

--- Sesion 9/Scripts/PlayerController.cs
+++ Sesion 9/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,28 +7,46 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D myRB;
     public float speed;
-    GameObject player;
+    //GameObject player;
+    bool faceRight = true;
+    SpriteRenderer myRenderer;
+    Animator myAnim;
     
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        // player = GameObject.FindGameObjectWithTag("Player");
+        myRenderer = GetComponent<SpriteRenderer>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
-        myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
-        if(move < 0)
+        
+        if (move > 0 && !faceRight)
         {
-            player.GetComponent<SpriteRenderer>().flipX = true;
+            // player.GetComponent<SpriteRenderer>().flipX = false;
+            Turn();
         }
-        if (move > 0)
+        else
         {
-            player.GetComponent<SpriteRenderer>().flipX = false;
+            if (move < 0 && faceRight)
+            {
+                // player.GetComponent<SpriteRenderer>().flipX = true;
+                Turn();
+            }
         }
 
+        myRB.velocity = new Vector2(move * speed, myRB.velocity.y);
+        myAnim.SetFloat("MoveSpeed", Mathf.Abs(move));
+    }
+
+    private void Turn()
+    {
+        myRenderer.flipX = !myRenderer.flipX;
+        faceRight = !faceRight;
     }
 }
